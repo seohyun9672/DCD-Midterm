48f1ecb0347cfecbb8dddb1476f999bbcad621fb
Anna Jeong <ajeong4@my.bcit.ca>
2022-02-25 07:19:40 -0800

    add comps.js

---
+++ comps/feelingbtn.js
@@ -0,0 +1,44 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_feelingbtn = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_feelingbtn.innerHTML = `
+<div class="feeling_btn" onclick="" hover="">
+    <span class="btn_text"> btn_text </span>
+</div>
+
+<style>
+    .feeling_btn {
+        padding: 5px;
+        width: fit-content;
+        height: fit-content;
+        border-radius: 5px;
+        background-color:#D9E694;
+    }
+</style>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheFeelingBtn extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_feelingbtn.content.cloneNode(true)); //use the template to make a clone
+        if(this.getAttribute("btn_text")) {
+            this.shadowRoot.querySelector("span").innerText = this.getAttribute("btn_text");
+        }
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("the-feelingbtn", TheFeelingBtn)
---
+++ comps/footprintbtn.js
@@ -0,0 +1,48 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_footprintbtn = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_footprintbtn.innerHTML = `
+<div class="footprint_btn">
+    <a href="nextpage.com">
+        <img src="bluefootprint.jpg" alt="footprint">
+    </a>
+</div>
+
+<style>
+    .footprint_btn {
+        border-radius: 50%;
+        border: white solid 10px;
+        background-color:transparent;
+    }
+    .footprint_btn > img {
+        display: block;
+        width: 64px;
+        height: 64px;
+        box-shadow: 3px 3px 1px #ccc;
+    }
+</style>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheFootprintBtn extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_footprintbtn.content.cloneNode(true)); //use the template to make a clone
+    
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("the-footprintbtn", TheFootprintBtn)
