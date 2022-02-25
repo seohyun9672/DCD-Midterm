125a052393259f8e4fec2a77963297dc0d9df3fe
Anna Jeong <ajeong4@my.bcit.ca>
2022-02-25 07:25:33 -0800

    add sources

--- comps/textcont.js
+++ comps/textcont.js
@@ -3,6 +3,9 @@ var template_textcont = document.createElement("template"); //<template> </templ
 
 //To-do - CREATE THE UI HERE!
 template_textcont.innerHTML = `
+    <script src="comps/time.js"></script>
+    <script src="comps/greenman.js"></script>
+    <script src="comps/chevron.js"></script>
     <div class="textcont">
         <the-chevron></the-chevron>
         <the-greenman></the-greenman>
