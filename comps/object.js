f9b803a5124dcd09b5cd9131aaabcb379b231979
cho196 <91933101+cho196@users.noreply.github.com>
2022-02-28 23:48:07 -0800

    object function no layout

--- comps/object.js
+++ comps/object.js
@@ -3,9 +3,17 @@ var template_object = document.createElement("template"); //<template> </templat
 
 //To-do - CREATE THE UI HERE!
 template_object.innerHTML = `
+<div class="obj-cont">
 <div class="object">
-    <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+    <img class="first" src="imgs/objects/phone.svg" alt="phone">
 </div>
+</div>
+<style>
+    div {
+        display:flex;
+        justify-content:center;
+        z-index:100;
+    }
 
 `;
 
@@ -25,20 +33,107 @@ class TheObject extends HTMLElement {
         // if (this.getAttribute("name")){
         //     document.querySelector(".object > .first").src = () => changeOb(this.getAttribute("name"));   
         // }
+        this.objNum = 1;
+        this.shadowRoot.querySelector("html").innerHTML = 
+        `<div class="object">
+        <img class="first" src="imgs/objects/phone.svg" alt="phone">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/car.svg" alt="car">
+        </div>`; 
     } 
         
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
-    changeOb(type = "story1"){
-        if(type === "story1"){
-        this.shadowRoot.querySelector(".object > .first").src = `/imgs/objects/phone.svg;`
-        // this.shadowRoot.querySelector(".object > .second").src = `/imgs/objects/car.svg;`
-        }
+    // changeOb(type = "story1"){
+    //     if(type === "story1"){
+    //     this.shadowRoot.querySelector(".object > .first").src = `/imgs/objects/phone.svg;`
+    //     // this.shadowRoot.querySelector(".object > .second").src = `/imgs/objects/car.svg;`
+    //     }
         
-        if(type === "story4"){
-        this.shadowRoot.querySelector(".object > .first").src = `/imgs/objects/tablet.svg;`
+    //     if(type === "story4"){
+    //     this.shadowRoot.querySelector(".object > .first").src = `/imgs/objects/tablet.svg;`
+    //     }
+
+    // }
+    updateObj() {
+        if(this.objNum === 1) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = 
+        `<div class="object">
+        <img class="first" src="imgs/objects/phone.svg" alt="phone">
+        </div>`; 
         }
 
+        if(this.objNum === 2) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = 
+        `<div class="object">
+        <img class="first" src="imgs/objects/phone.svg" alt="phone">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/car.svg" alt="car">
+        </div>`;  
+        }
+        
+        if(this.objNum === 3) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+        </div>`;
+        }
+        
+        if(this.objNum === 4) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/fan.svg" alt="fan">
+        </div>`;  
+        }
+        if(this.objNum === 5) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/fan.svg" alt="fan">
+        </div>
+        <div class="object">
+        <img class="third" src="imgs/objects/screen.svg" alt="screen">
+        </div>`;   
+        }
+        if(this.objNum === 6) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
+        </div>`;  
+        }
+        if(this.objNum === 7) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/laundrybasket.svg" alt="basket">
+        </div>`;  
+        }
+        if(this.objNum === 8) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/tablet.svg" alt="tablet">
+        </div>`;  
+        }
+        if(this.objNum === 9) {
+        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+        <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
+        </div>
+        <div class="object">
+        <img class="second" src="imgs/objects/switch.svg" alt="switch">
+        </div>`;  
+        }
+    }
+    remObj() {
+        this.objNum = this.objNum - 1;
+        this.updateObj();
+    }
+    
+    addObj() {
+        this.objNum = this.objNum + 1;
+        this.updateObj();
     }
 }
 
