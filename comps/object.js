ca779516eb2e206be661ea5223458f5579dae0c2
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-01 18:43:26 -0800

    edit object html

--- comps/object.js
+++ comps/object.js
@@ -3,16 +3,15 @@ var template_object = document.createElement("template"); //<template> </templat
 
 //To-do - CREATE THE UI HERE!
 template_object.innerHTML = `
-<div class="obj-cont">
 <div class="object">
-    <img class="first" src="imgs/objects/phone.svg" alt="phone">
-</div>
+    <img src="imgs/objects/phone.svg" alt="phone">
 </div>
 <style>
     .object {
      position: absolute;
-     top: 500px;   
-     left: 750px
+     top: 400px;   
+     left: 600px;
+     cursor: pointer;
     }
 `;
 
@@ -33,263 +32,265 @@ class TheObject extends HTMLElement {
         //     document.querySelector(".object > .first").src = () => changeOb(this.getAttribute("name"));   
         // }
         this.objNum = 1;
-        this.shadowRoot.querySelector(".obj-cont").onclick = () => {
+        this.shadowRoot.querySelector(".object > img").onclick = () => {
             // this.trigger();
-            // this.popUp();
             document.querySelector("the-infocard").showInfocard();
+            // this.changeOb(); //testing 
         }
-        
     } 
- 
-
-
-    updateObj() {
-        if(this.objNum === 1) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = 
-        `<div class="object">
-        <img class="first" src="imgs/objects/phone.svg" alt="phone">
-        </div>
-        <style>
-        .first {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`; 
-        }
-
-        if(this.objNum === 2) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = 
-        `<div class="object">
-        <img class="first" src="imgs/objects/phone.svg" alt="phone">
-        </div>
-        <div class="object">
-        <img class="second" src="imgs/objects/car.svg" alt="car">
-        </div>
-        <style>
-        .second {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-        
-        if(this.objNum === 3) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
-        </div>
-        <style>
-        .first {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;
-        }
-        
-        if(this.objNum === 4) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
-        </div>
-        <div class="object">
-        <img class="second" src="imgs/objects/fan.svg" alt="fan">
-        </div>
-        <style>
-        .second {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-        if(this.objNum === 5) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
-        </div>
-        <div class="object">
-        <img class="second" src="imgs/objects/fan.svg" alt="fan">
-        </div>
-        <div class="object">
-        <img class="third" src="imgs/objects/screen.svg" alt="screen">
-        </div>
-        <style>
-        .third {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;   
-        }
-        if(this.objNum === 6) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
-        </div>
-        <style>
-        .first {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-        if(this.objNum === 7) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
-        </div>
-        <div class="object">
-        <img class="second" src="imgs/objects/laundrybasket.svg" alt="basket">
-        </div>
-        <style>
-        .second {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-        if(this.objNum === 8) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/tablet.svg" alt="tablet">
-        </div>
-        <style>
-        .first {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-        if(this.objNum === 9) {
-        this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
-        <img class="first" src="imgs/objects/tablet.svg" alt="tablet">
-        </div>
-        <div class="object">
-        <img class="second" src="imgs/objects/switch.svg" alt="switch">
-        </div>
-        <style>
-        .second {
-            animation: 3s fadeIn;
-            animation-fill-mode: forwards;
-            
-            visibility: hidden;
-          }
-          
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              visibility: visible;
-              opacity: 1;
-            }
-          }
-        </style>`;  
-        }
-    }
-    remObj() {
-        this.objNum = this.objNum - 1;
-        this.updateObj();
+    changeOb(){
+      this.shadowRoot.querySelector(".object > img").src = `./imgs/objects/${this.getAttribute("ob_name")}.svg`
     }
     
-    addObj() {
-        this.objNum = this.objNum + 1;
-        this.updateObj();
-    }
-    trigger() {
+
+
+    // updateObj() {
+    //     if(this.objNum === 1) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = 
+    //     `<div class="object">
+    //     <img class="first" src="imgs/objects/phone.svg" alt="phone">
+    //     </div>
+    //     <style>
+    //     .first {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`; 
+    //     }
+
+    //     if(this.objNum === 2) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = 
+    //     `<div class="object">
+    //     <img class="first" src="imgs/objects/phone.svg" alt="phone">
+    //     </div>
+    //     <div class="object">
+    //     <img class="second" src="imgs/objects/car.svg" alt="car">
+    //     </div>
+    //     <style>
+    //     .second {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+        
+    //     if(this.objNum === 3) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+    //     </div>
+    //     <style>
+    //     .first {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;
+    //     }
+        
+    //     if(this.objNum === 4) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+    //     </div>
+    //     <div class="object">
+    //     <img class="second" src="imgs/objects/fan.svg" alt="fan">
+    //     </div>
+    //     <style>
+    //     .second {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+    //     if(this.objNum === 5) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/waterbottle.svg" alt="waterbottle">
+    //     </div>
+    //     <div class="object">
+    //     <img class="second" src="imgs/objects/fan.svg" alt="fan">
+    //     </div>
+    //     <div class="object">
+    //     <img class="third" src="imgs/objects/screen.svg" alt="screen">
+    //     </div>
+    //     <style>
+    //     .third {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;   
+    //     }
+    //     if(this.objNum === 6) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
+    //     </div>
+    //     <style>
+    //     .first {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+    //     if(this.objNum === 7) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/laundrydetergent.svg" alt="detergent">
+    //     </div>
+    //     <div class="object">
+    //     <img class="second" src="imgs/objects/laundrybasket.svg" alt="basket">
+    //     </div>
+    //     <style>
+    //     .second {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+    //     if(this.objNum === 8) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/tablet.svg" alt="tablet">
+    //     </div>
+    //     <style>
+    //     .first {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+    //     if(this.objNum === 9) {
+    //     this.shadowRoot.querySelector(".obj-cont").innerHTML = `<div class="object">
+    //     <img class="first" src="imgs/objects/tablet.svg" alt="tablet">
+    //     </div>
+    //     <div class="object">
+    //     <img class="second" src="imgs/objects/switch.svg" alt="switch">
+    //     </div>
+    //     <style>
+    //     .second {
+    //         animation: 3s fadeIn;
+    //         animation-fill-mode: forwards;
+            
+    //         visibility: hidden;
+    //       }
+          
+    //       @keyframes fadeIn {
+    //         0% {
+    //           opacity: 0;
+    //         }
+    //         100% {
+    //           visibility: visible;
+    //           opacity: 1;
+    //         }
+    //       }
+    //     </style>`;  
+    //     }
+    // }
+    // remObj() {
+    //     this.objNum = this.objNum - 1;
+    //     this.updateObj();
+    // }
+    
+    // addObj() {
+    //     this.objNum = this.objNum + 1;
+    //     this.updateObj();
+    // }
+    // 
+    // }
+trigger() {
         document.querySelector("the-textcont").increaseTextCont();
     }
-
-}
-
+    }
 //MUST HAVE - define the tag for the custom elements
 customElements.define("the-object", TheObject)
 
