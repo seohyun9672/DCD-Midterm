3e1240a4f155ebb951aa4d12cc40c916da4eca0d
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-02 07:15:20 -0800

    update infocard

--- comps/infocard.js
+++ comps/infocard.js
@@ -3,27 +3,18 @@ var template_infocard = document.createElement("template"); //<template> </templ
 
 //To-do - CREATE THE UI HERE!
 template_infocard.innerHTML = `
-<script src="comps/textcont.js"></script>
 <div class="infocard-cont">
-    <div>
-        
-        <p class="main-text">Number of</p>
-        <em> SMARTPHONE WASTED </em>
-        <p class="main-text">every year</p>
-        <img class="info-img" src="imgs/infoimg/card_phone.svg" alt="">
-        
-        <the-textbtn
-        btn_text ="Got it!"
-        color="green"
-        class ="gotit"
-        ><the-textbtn>
-        <p class="desc-text"></p>
-        
-    </div>
+    <p> Number of <br> SMARTPHONE WASTED <br> every year </p>
+    <img class="info-img" src="imgs/infoimg/card_phone.svg" alt="">
+    <the-textbtn
+    btn_text ="Got it!"
+    class ="gotit"
+    color= "green"
+    ><the-textbtn>
 </div>
 <style>
     .infocard-cont {
-        background-color:#FFF;
+        background-color:#FFFFFF;
         border: none;
         border-radius:5pt;
         width: 230px;
@@ -32,27 +23,19 @@ template_infocard.innerHTML = `
         padding:1em;    
         display: none;
         position: relative;
-       
-       
+        text-align: center;
     }
-    .infocard-cont > div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin: 0;
-        width: 230px;
-        height: 320px;
-        
-        // line-height: 0.4em;
-    }
-    .main-text, em {
+    p {
         font-family:"ibm plex sans", "Ubuntu", sans-serif;
         text-transform:uppercase;
         font-weight:bold;
         letter-spacing:2.26pt;
-        font-style:normal;
+        line-height: 2em;
+        margin-top:.5em;
     }
-
+    .info-img {
+        margin-bottom: 1em;
+    }
 </style>
 `;
 
@@ -70,31 +53,30 @@ class TheInfoCard extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(template_infocard.content.cloneNode(true)); //use the template to make a clone
-        if(this.getAttribute("num_text")){
-            this.shadowRoot.querySelector(".num-text").innerText = this.getAttribute("num_text");
-        }
-        if(this.getAttribute("main_text")){
-            this.shadowRoot.querySelector(".main-text").innerText = this.getAttribute("main_text");
-        }    
-        if(this.getAttribute("desc_text")){
-            this.shadowRoot.querySelector(".desc-text").innerText = this.getAttribute("desc_text");
-        }   
-        if(this.getAttribute("capt_text")){
-            this.shadowRoot.querySelector(".capt-text").innerText = this.getAttribute("capt_text");
-        }
-        if(this.getAttribute("em_text")){
-        this.shadowRoot.querySelector("em").innerText = this.getAttribute("em_text");
-        }
-        if(this.getAttribute("em_color")){
-        this.shadowRoot.querySelector("em").style.color = this.getAttribute("em_color");
-        }
-        if(this.getAttribute("src")){
-        this.shadowRoot.querySelector("src.info-img").src = `imgs/infoimg/card_${phone}.svg`;
-        }
-        if(this.getAttribute("btn_text")){
-        this.shadowRoot.querySelector("the-textbtn").innerText = this.getAttribute("btn_text");
-        }
-        this.shadowRoot.querySelector(".gotit").onclick = () => document.querySelector(".textbtn_cont").trigger();
+        
+        // change innerHtml for each infocard 
+        
+        // if(this.getAttribute("src")){
+        //     this.shadowRoot.querySelector("src.info-img").src = `imgs/infoimg/card_${phone}.svg`;
+        // }
+        // if(this.getAttribute("num_text")){
+        //     this.shadowRoot.querySelector(".num-text").innerText = this.getAttribute("num_text");
+        // }
+        // if(this.getAttribute("main_text")){
+        //     this.shadowRoot.querySelector(".main-text").innerText = this.getAttribute("main_text");
+        // }    
+        // if(this.getAttribute("desc_text")){
+        //     this.shadowRoot.querySelector(".desc-text").innerText = this.getAttribute("desc_text");
+        // }   
+        // if(this.getAttribute("capt_text")){
+        //     this.shadowRoot.querySelector(".capt-text").innerText = this.getAttribute("capt_text");
+        // }
+        // if(this.getAttribute("em_text")){
+        // this.shadowRoot.querySelector("em").innerText = this.getAttribute("em_text");
+        // }
+        // if(this.getAttribute("em_color")){
+        // this.shadowRoot.querySelector("em").style.color = this.getAttribute("em_color");
+        // }
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
@@ -103,6 +85,11 @@ class TheInfoCard extends HTMLElement {
     }
     removeInfoCard(){
         this.shadowRoot.querySelector(".infocard-cont").style.display = "none";
+    }
+    updateInfoCard(){
+        // if click on the different object, the different info card shows up
+        // name it infocard_num (smth like this) i.e. this.infoNum = 1
+        // so that if we click on the different object, infocard_num will show up
     }
     
 }
