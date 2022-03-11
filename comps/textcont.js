805a183a80d0ebd433b9a8b7ec30c94dc8873366
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-11 12:18:17 -0800

    edit functionalities for the textcont

--- comps/textcont.js
+++ comps/textcont.js
@@ -22,11 +22,10 @@ template_textcont.innerHTML = `
 
 //MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
 class TheTextcont extends HTMLElement {
-
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
     constructor() {
         super(); //pass on the HTMLElement super powers!
-        this.attachShadow({ mode: "open" }) //Attach it to the shadowRoot
+        this.attachShadow({ mode: "open" }); //Attach it to the shadowRoot
 
         //To-do - CREATE THE STATES FOR THE UI HERE!
     }
@@ -36,66 +35,99 @@ class TheTextcont extends HTMLElement {
         this.shadowRoot.appendChild(template_textcont.content.cloneNode(true)); //use the template to make a clone
 
         this.textOrder = 1;
-        document.querySelector(".textcont").UpdateStoryUI();
+        document.querySelector(".textcont").updateStoryUI();
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
-    styleEmText(textcolor = "red"){
-        this.shadowRoot.querySelector("span").style.cssText = `
-        color: ${textcolor};
-        font-weight: bold;
-        `
-    }
-
-    UpdateStoryUI(textOrder = 1) {
+    updateStoryUI(textOrder = 1) {
         if (this.textOrder === 1) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
             Mr. Green wakes up in the morning with an alarm from his brand new phone. <br>
             Click on his <span>phone</span> to turn the alarm off.`;
-            this.shadowRoot.querySelector("span").styleEmText(textcolor = "red");
-            // this.shadowRoot.querySelector("span").style.color ="red";
+            this.shadowRoot.querySelector("span").style.cssText = `
+            color: darkgrey;
+            font-weight: bold;
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle1");
+            document.querySelector(".storybg").changeStory(type === "story1");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman1");
         }
-
         if (this.textOrder === 2) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Great! Now Mr. Green is ready to go to work.
-        Click on his car to get it started. `
-
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            Great! Now Mr. Green is ready to go to work. <br> 
+            Click on his <span>car</span> to get it started.`;
+            this.shadowRoot.querySelector("span").style.cssText = `
+            color: yellow;
+            font-weight: bold;
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle1");
+            document.querySelector(".storybg").changeStory(type === "story1");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman2");
         }
         if (this.textOrder === 3) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `His office is quite hot. He grabs a water bottle from the vending machine to cool down,
-        Click on the bottle to help Mr. Green quench his thirst. `
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            His office is quite hot. He grabs a water bottle from the vending machine to cool down. <br>
+            Click on the <span>bottle</span> to help Mr. Green quench his thirst. `;
+            this.shadowRoot.querySelector("span").style.cssText = `
+            color: skyblue;
+            font-weight: bold;
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle2");
+            document.querySelector(".storybg").changeStory(type === "story2");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman2");
         }
         if (this.textOrder === 4) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Great! However, the water doesn’t seem to help. He wants to turn on his fan.
-        Click on his desk fan to help him cool down.`
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            Great! However, the water doesn’t seem to help. He wants to turn on his fan. <br>
+            Click on his desk <span>fan</span> to help him cool down.
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle3");
+            document.querySelector(".storybg").changeStory(type === "story2");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman3");
         }
         if (this.textOrder === 5) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Mr. Green is feeling cool now!
-        Click on Mr. Green’s computer screen to get his work day started.`
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            Mr. Green is feeling cool now! <br>
+            Click on Mr. Green’s computer <span>screen</span> to get his work day started.
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle3");
+            document.querySelector(".storybg").changeStory(type === "story2");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman3");
         }
         if (this.textOrder === 6) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Mr. Green usually does his laundry after work.
-        Click on the laundry detergent to help Mr. Green start his laundry.`
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            Mr. Green usually does his laundry after work. <br>
+            Click on the laundry detergent to help Mr. Green start his laundry.
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle4");
+            document.querySelector(".storybg").changeStory(type === "story3");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman4");
         }
         if (this.textOrder === 7) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Mr. Green finishes his laundry.
-        Click on the laundry basket to help him throw his clothes into the dryer. `
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            Mr. Green finishes his laundry. <br>
+            Click on the <span>laundry basket</span> to help him throw his clothes into the dryer. 
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle4");
+            document.querySelector(".storybg").changeStory(type === "story3");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman4");
         }
         if (this.textOrder === 8) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `While folding his laundry, Mr. Green remembers the movie that his collegue recommended. 
-        Click on the tablet to watch Netflix.`
+            this.shadowRoot.querySelector(".textcont > p").innerHTML = `
+            While folding his laundry, Mr. Green remembers the movie that his colleague recommended. <br>
+            Click on the <span>tablet</span> to watch Netflix.`;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle5");
+            document.querySelector(".storybg").changeStory(type === "story4");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman5");
         }
         if (this.textOrder === 9) {
-            this.shadowRoot.querySelector(".textcont > p").innerText =
-                `Mr. Green fell asleep on the couch while watching TV.
-        It seems that he forgot to turn off the light. Click on the switch to turn off the light for him.`
+            this.shadowRoot.querySelector(".textcont > p").innerText = `
+            Mr. Green fell asleep on the couch while watching TV. It seems that he forgot to turn off the light. <br>
+            Click on the switch to turn off the light for him.
+            `;
+            document.querySelector(".cycleimg").changeFootprintCycle(type === "footprintcycle6");
+            document.querySelector(".storybg").changeStory(type === "story4");
+            document.querySelector(".greenmanimg").changeGreenman(type === "greenman6");
         }
         if (this.textOrder === 10) {
             window.location.href = "/cta.html";
@@ -104,15 +136,15 @@ class TheTextcont extends HTMLElement {
     previousTextCont() {
         this.textOrder = this.textOrder - 1;
         document.querySelector("the-time").decreaseTime();
-        this.updateTextCont();
+        this.updateStoryUI();
     }
 
     nextTextCont() {
         this.textOrder = this.textOrder + 1;
         document.querySelector("the-time").increaseTime();
-        this.updateTextCont();
+        this.updateStoryUI();
     }
 }
 
 //MUST HAVE - define the tag for the custom elements
-customElements.define("the-textcont", TheTextcont)
+customElements.define("the-textcont", TheTextcont);
