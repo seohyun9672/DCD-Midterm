602de8aa28b2f9e9485b36ae1a15810ecc3d787c
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-12 10:00:58 -0800

    fix greenman functionalities

--- comps/greenman.js
+++ comps/greenman.js
@@ -4,63 +4,51 @@ var template_greenman = document.createElement("template"); //<template> </templ
 //To-do - CREATE THE UI HERE!
 template_greenman.innerHTML = `
 <div class = "greenman">
-    <img src="./imgs/greenman/Greenman1.svg" alt=greenman>
+    <img src="./imgs/greenman/Greenman1.svg" alt= Greenman Image>
 </div>
-
-<style>
-    .greenman > img {
-        padding-right: 2.2em;
-    }
-</style>
 `;
 
 //MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
 class TheGreenman extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
-    constructor(){
+    constructor() {
         super(); //pass on the HTMLElement super powers!
-        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+        this.attachShadow({ mode: "open" }) //Attach it to the shadowRoot
 
         //To-do - CREATE THE STATES FOR THE UI HERE!
     }
-    
+
 
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
-    connectedCallback(){
+    connectedCallback() {
         this.shadowRoot.appendChild(template_greenman.content.cloneNode(true)); //use the template to make a clone
-        
-        if(this.getAttribute("direction")){
-            this.shadowRoot.querySelector(".greenman").style.transform= "scaleX(-1)";
+
+        if (this.getAttribute("direction")) {
+            this.shadowRoot.querySelector(".greenman").style.transform = "scaleX(-1)";
         }
-        // if(this.getAttribute("greenman_num")){
-        //     this.shadowRoot.querySelector(".greenman").src=this.changeGreenman();
-        // }
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
-    changeGreenman(type="greenman_num"){
-        this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
-        if(type==="greenman1"){
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
+    changeGreenman(type = "greenman_num") {
+        if (type === "greenman1") {
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman1.svg`;
         }
         if (type === "greenman2") {
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/greenman2.svg`;
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman2.svg`;
         }
-        if(type==="greenman3"){
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
+        if (type === "greenman3") {
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman3.svg`;
         }
-        if(type==="greenman4"){
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
+        if (type === "greenman4") {
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman4.svg`;
         }
-        if(type==="greenman5"){
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
+        if (type === "greenman5") {
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman5.svg`;
         }
-        if(type==="greenman6"){
-            this.shadowRoot.querySelector(".greenman > img").src=`./imgs/Greenman/${this.getAttribute("greenman_num")}.svg`;
+        if (type === "greenman6") {
+            this.shadowRoot.querySelector(".greenman > img").src = `./imgs/Greenman/greenman6.svg`;
         }
-        
-
     }
 }
 
