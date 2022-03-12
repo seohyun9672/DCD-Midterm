307f3d480867f0927fd683d4a71b38a58671dfee
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-12 12:39:31 -0800

    update updateStoryUI function

--- comps/textcont.js
+++ comps/textcont.js
@@ -39,7 +39,7 @@ class TheTextcont extends HTMLElement {
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
-    updateStoryUI(textOrder = 1) {
+    updateStoryUI() {
         if (this.textOrder === 1) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
             Mr. Green wakes up in the morning with an alarm from his brand new phone. <br>
@@ -61,10 +61,11 @@ class TheTextcont extends HTMLElement {
             color: yellow;
             font-weight: bold;
             `;
-            document.querySelector(".cycleimg").changeFootprintCycle("footprintcycle1");
+            document.querySelector(".cycleimg").changeFootprintCycle("footprintcycle2");
             document.querySelector(".storybg").changeStory("story1");
             document.querySelector(".greenmanimg").changeGreenman("greenman2");
             document.querySelector("the-object").addObj("car");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("car");
         }
         if (this.textOrder === 3) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -78,6 +79,8 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story2");
             document.querySelector(".greenmanimg").changeGreenman("greenman2");
             document.querySelector("the-object").addObj("waterbottle");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("waterbottle");
+
         }
         if (this.textOrder === 4) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -88,6 +91,7 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story2");
             document.querySelector(".greenmanimg").changeGreenman("greenman3");
             document.querySelector("the-object").addObj("fan");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("fan");
         }
         if (this.textOrder === 5) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -98,6 +102,7 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story2");
             document.querySelector(".greenmanimg").changeGreenman("greenman3");
             document.querySelector("the-object").addObj("screen");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("screen");
         }
         if (this.textOrder === 6) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -108,6 +113,7 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story3");
             document.querySelector(".greenmanimg").changeGreenman("greenman4");
             document.querySelector("the-object").addObj("laundrydetergent");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("laundrydetergent");
         }
         if (this.textOrder === 7) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -118,6 +124,8 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story3");
             document.querySelector(".greenmanimg").changeGreenman("greenman4");
             document.querySelector("the-object").addObj("laundrybasket");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("laundrybasket");
+            
         }
         if (this.textOrder === 8) {
             this.shadowRoot.querySelector(".textcont > p").innerHTML = `
@@ -127,6 +135,7 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story4");
             document.querySelector(".greenmanimg").changeGreenman("greenman5");
             document.querySelector("the-object").addObj("tablet");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("tablet");
         }
         if (this.textOrder === 9) {
             this.shadowRoot.querySelector(".textcont > p").innerText = `
@@ -137,6 +146,7 @@ class TheTextcont extends HTMLElement {
             document.querySelector(".storybg").changeStory("story4");
             document.querySelector(".greenmanimg").changeGreenman("greenman6");
             document.querySelector("the-object").addObj("switch");
+            document.querySelector("the-object").onclick = () => document.querySelector("the-infocard").changeInfoCard("switch");
         }
         if (this.textOrder === 10) {
             window.location.href = "/cta.html";
