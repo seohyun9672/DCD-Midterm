ec976cc3b661fb7ea4348bd914b0886f248ccee3
seohyun9672 <ajeong4@my.bcit.ca>
2022-03-13 17:58:46 -0700

    change image size

--- comps/footprintbtn.js
+++ comps/footprintbtn.js
@@ -4,27 +4,26 @@ var template_footprintbtn = document.createElement("template"); //<template> </t
 //To-do - CREATE THE UI HERE!
 template_footprintbtn.innerHTML = `
 <div class="footprint_btn">
-    <a href="/story.html">
-        <img src="imgs/bluefootprint.svg" alt="footprint">
-    </a>
+    <img src="imgs/bluefootprint.svg" alt="Footprint">
 </div>
 
 <style>
     .footprint_btn {
         border-radius: 50%;
-        border: white solid 20px;
+        border: white solid 40px;
         background-color:transparent;
-        width: 200px;
-        height: 200px;
-        margin-top:2em;
+        width: 300pt;
+        height: 300pt;
         display: flex;
         align-items:center;
         justify-content:center;
         box-shadow: 0 4px 20px -1px #E2E2E2;
+        cursor: pointer;
     }
-    .footprint_btn img {
-        height: 90%;
-        width: 90%;
+    .footprint_btn > img {
+        width: 80%;
+        height: 80%;
+        z-index: 1;
     }
 </style>
 `;
@@ -33,17 +32,17 @@ template_footprintbtn.innerHTML = `
 class TheFootprintBtn extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
-    constructor(){
+    constructor() {
         super(); //pass on the HTMLElement super powers!
-        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+        this.attachShadow({ mode: "open" }) //Attach it to the shadowRoot
 
         //To-do - CREATE THE STATES FOR THE UI HERE!
     }
 
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
-    connectedCallback(){
+    connectedCallback() {
         this.shadowRoot.appendChild(template_footprintbtn.content.cloneNode(true)); //use the template to make a clone
-    
+        this.shadowRoot.querySelector(".footprint_btn").onclick = () => document.location.href = "/story.html";
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
