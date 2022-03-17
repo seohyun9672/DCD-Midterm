3f2e6979b69902a752ce186f87362226945a7b15
SueLee <slee1008@my.bcit.ca>
2022-03-16 21:38:39 -0700

    adjust greenman's size for cta

--- comps/greenman.js
+++ comps/greenman.js
@@ -43,11 +43,14 @@ class TheGreenman extends HTMLElement {
         if (this.getAttribute("size")) {
             this.shadowRoot.querySelector(".greenman >img").style.height = "160pt";
         }
+        
         let mediaQuery = window.matchMedia("(max-width: 1440px)");
         if (mediaQuery.matches){
                 this.shadowRoot.querySelector(".greenman > img").style.height = "110pt";
         }
-        
+        if (this.getAttribute("size2")) {
+            this.shadowRoot.querySelector(".greenman >img").style.height = "200pt";
+        }
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
